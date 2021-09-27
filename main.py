24107ccfd5fb6e29b473a2568244c251ae473ce5
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-27 21:02:34 +0530

    Added receive caption support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pyromod import listen
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
@@ -9,14 +10,32 @@ bughunter0 = Client(
      api_hash = os.environ["API_HASH"]
 )
 
-CAPTION=""" Add Your Caption Here """
+CAPTION = os.environ.get("CAPTION", None)
 
 # Better to add caption through config vars / app.json
 
 
 @bughunter0.on_message(filters.media)
-async def caption(bot,message):
-	chatid=message.chat.id
-	await message.copy(chat_id=chatid,caption=CAPTION)
-	
+async def caption(bot, message):
+    chat_id = message.chat.id
+    if CAPTION:
+        caption = CAPTION
+    else:
+        caption = await get_caption(message)
+        return if caption is True else pass
+    await message.copy(chat_id=chatid, caption=caption)
+
+
+async def get_caption(message):
+    caption = await bot.ask("Send a caption for the media")
+    if not caption.text:
+        await caption.reply("No caption found", quote=True)
+        return await get_caption(message)
+    if caption.text.startswith("/cancel"):
+        await caption.reply("Process cancelled", quote=True)
+        return True
+    else:
+        return caption.text
+
+
 bughunter0.run()
